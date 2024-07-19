37b40cc025ff31704f3350e398f594bbe9b4eb1a
iman hashemi <hashemi.iman@gmail.com>
2024-07-19 18:17:04 +0330

    image Extention Bug

--- app/Helpers/ImageManager.php
+++ app/Helpers/ImageManager.php
@@ -4,20 +4,23 @@ namespace App\Helpers;
 
 use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Drivers\Gd\Driver;
+
+use Intervention\Image\Drivers\Gd\Encoders\PngEncoder;
 use Intervention\Image\ImageManager as IM;
+use Intervention\Image\Interfaces\EncoderInterface;
 
 class ImageManager
 {
     public static function saveImage($table,$image){
         if($image){
-            $name = $image->hashName();
+            $name = pathinfo($image->hashName() , PATHINFO_FILENAME).'.png';
             $manager = new IM(new Driver());
             $smallImage = $manager->read($image->getRealPath());
             $bigImage = $manager->read($image->getRealPath());
             $smallImage->resize(256,256);
 
-            Storage::disk('local')->put($table.'/small/'.$name, (string) $smallImage->toPng());
-            Storage::disk('local')->put($table.'/big/'.$name, (string) $bigImage->toPng());
+            Storage::disk('local')->put($table.'/small/'.$name, (string) $smallImage->encode());
+            Storage::disk('local')->put($table.'/big/'.$name, (string) $bigImage->encode());
             return $name;
         }else{
             return "";
@@ -37,7 +40,7 @@ class ImageManager
         $name = $image->hashName();
         $manager = new IM(new Driver());
         $bigImage = $manager->read($image->getRealPath());
-        Storage::disk('local')->put($table.'/big/'.$name, (string) $bigImage->toPng());
+        Storage::disk('local')->put($table.'/big/'.$name, (string) $bigImage->encode());
         return url("images/$table/big/".$name);
 
     }
